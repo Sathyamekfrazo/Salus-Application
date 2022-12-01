96ab84a623241d6de77efd7ab593ca27ff586d40
sathyam@123 <sathyam@ekfrazo.com>
2022-12-01 10:47:58 +0530

    adding project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      { path: '', redirectTo: 'outer', pathMatch: 'full' },
+      {
+        path: 'main',
+        loadChildren: () =>
+          import('./main/main.module').then((m) => m.MainModule),
+          // canActivate:[AuthGuardService]
+      },
+      {
+        path: 'outer',
+        loadChildren: () =>
+          import('./outer/outer.module').then((m) => m.OuterModule),
+      },
+    ]
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
